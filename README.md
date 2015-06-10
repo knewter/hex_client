b61cc05d2847f6fa3a3e037934edef30ca57e62f
Julius Beckmann <github@h4cc.de>
2015-06-10 22:06:56 +0200

    Implement all public Hex API calls. Wrote some tests. Added Travis Builds.

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-HexClient
-=========
+# HexClient
 
-This is a tiny API client for the hex.pm API, currently only enabling the things
-that the BEAM Toolbox needs to do.
+This is a simple API client for the [hex.pm](https://hex.pm/) [API](https://github.com/hexpm/hex_web/blob/master/lib/hex_web/api/router.ex),
+currently only able to fetch the public information not needing authentication.
 
 So far you can:
 
-- list packages
-- that's it for now :)
+- list packages as stream ordered by "name", "downloads", "inserted_at" or "updated_at".
+- list all releases for a package.
+- detailed information about a single package release.
+- documentation archive download link for package release.
--- lib/hex_client.ex
+++ lib/hex_client.ex
@@ -2,23 +2,102 @@ defmodule HexClient do
   use HTTPoison.Base
 
   @base_url "https://hex.pm/api/"
+  @packages_sort "name"
 
-  # Get all packages, iterating over each page
+  @doc """
+    Will fetch all packages from hex ordered by name.
+
+    Returns: A list of Packages.
+  """
   def packages do
-    do_packages(1, packages(1), [])
+    packages_stream |> Enum.to_list
   end
 
-  # Iterate over each page of packages, stopping when the result are empty for a given page
-  defp do_packages(page, [], acc), do: acc
-  defp do_packages(page, results, acc) do
-    do_packages(page+1, packages(page+1), acc ++ results)
+  # Helper for newest packages.
+  def packages_newest(amount \\ 100) do
+    packages_stream("inserted_at") |> Enum.take(amount)
   end
+
+  # Helper for most downloaded.
+  def packages_most_downloaded(amount \\ 100) do
+    packages_stream("downloads") |> Enum.take(amount)
+  end
+
+  @doc """
+    Will fetch the packages from hex ordered by given sort key.
+
+    Sortable by:
+      name downloads inserted_at updated_at
+
+    Returns: A Stream of Packages.
+  """
+  def packages_stream(sort \\ @packages_sort) do
+    # Starting from page number 1 up,
+    # asking the API till we reach first empty page.
+    Stream.unfold(1, fn page ->
+      case packages_page(page, sort) do
+        # No more packages
+        [] -> nil
+        # Error occured
+        nil -> nil
+        # Request next page with next unfold
+        body -> {body, page+1}
+      end
+    end)
+    |> Stream.flat_map(fn x -> x end)
+  end
+
+  @doc """
+    Will fetch a package by name.
+
+    Returns: The data for the package or nil.
+  """
+  def package(name) do
+    package_path(name) |> get_body
+  end
+
+  @doc """
+    Will fetch info for a package version.
+
+    Returns: The data for the package version or nil.
+  """
+  def package(name, version) do
+    package_path(name, version) |> get_body
+  end
+
+  @doc """
+    Will fetch the url for downloading docs.
+
+    Returns: The data for the package version or nil.
+  """
+  def package_docs_download(name, version) do
+    {:ok, %HTTPoison.Response{headers: headers, status_code: status_code}} = package_docs_path(name, version) |> get
+    case status_code do
+      302 ->
+        %{"Location"=> url} = headers
+        url
+      _ -> nil
+    end
+  end
+
+  # TODO: Implement calls using authentication.
 
   # Get a given page of packags
-  defp packages(page), do: packages_path(page) |> get_body
+  defp packages_page(page, sort) do
+    packages_path(page, sort) |> get_body
+  end
 
-  defp get_body(path), do: get(path).body
-  defp packages_path(page), do: "packages?page=#{page}"
-  def process_url(url), do: @base_url <> url
-  def process_response_body(body), do: Poison.decode!(body)
+  # Fetch the Response body for given path or return nil
+  defp get_body(path) do
+    case get(path) do
+      {:ok, %HTTPoison.Response{body: body, status_code: 200}} -> body |> Poison.decode!
+      _ -> nil
+    end
+  end
+
+  defp packages_path(page, sort), do: "packages?page=#{page}&sort=#{sort}"
+  defp package_path(name), do: "packages/#{name}"
+  defp package_path(name, version), do: "packages/#{name}/releases/#{version}"
+  defp package_docs_path(name, version), do: "packages/#{name}/releases/#{version}/docs"
+  defp process_url(url), do: @base_url <> url
 end
